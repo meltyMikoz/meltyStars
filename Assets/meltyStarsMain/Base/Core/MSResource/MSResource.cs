aa555f248ec2786e4fc48a727b1cd649e14ba662
meltyMikoz <3190748326@qq.com>
2022-06-14 19:26:37 +0800

    update

--- Assets/meltyStarsMain/Base/Core/MSResource/MSResource.cs
+++ Assets/meltyStarsMain/Base/Core/MSResource/MSResource.cs
@@ -14,13 +14,18 @@ namespace meltyStars
         /// 封装AsyncOperationHandle
         /// </summary>
         /// <typeparam name="TObject"></typeparam>
-        public class MSResourceRequestHandle<TObject> : IEquatable<MSResourceRequestHandle<TObject>> where TObject : UnityEngine.Object
+        public class MSResourceRequestHandle<TObject> : IEquatable<MSResourceRequestHandle<TObject>>, IDisposable where TObject : UnityEngine.Object
         {
             private AsyncOperationHandle<TObject> asyncOperationHandle;
             public AsyncOperationHandle<TObject> AsyncOperationHandle => asyncOperationHandle;
-            private MSResourceRequestHandle(AsyncOperationHandle<TObject> handle)
+            private MSResourceRequestHandle(AsyncOperationHandle<TObject> asyncOperationHandle)
             {
-                this.asyncOperationHandle = handle;
+                this.asyncOperationHandle = asyncOperationHandle;
+                asyncOperationHandle.Completed += handle =>
+                {
+                    OnCompleted.Invoke(this.Result);
+                    OnCompletedHandle.Invoke(this);
+                };
             }
             public bool IsDone => asyncOperationHandle.IsDone;
             public Exception OperationException => asyncOperationHandle.OperationException;
@@ -28,16 +33,9 @@ namespace meltyStars
             public TObject Result => asyncOperationHandle.Result;
             public AsyncOperationStatus Status => asyncOperationHandle.Status;
             public Task<TObject> Task => asyncOperationHandle.Task;
-
-            public event Action<MSResourceRequestHandle<TObject>> OnCompleted;
-            public event Action OnCompletedTypeless;
-            public event Action OnDestroyed;
+            public event Action<TObject> OnCompleted;
+            public event Action<MSResourceRequestHandle<TObject>> OnCompletedHandle;
             public TObject WaitForCompletion() => asyncOperationHandle.WaitForCompletion();
-            public TObject WaitForCompletionTask()
-            {
-                Task.Wait();
-                return Task.Result;
-            }
             public static MSResourceRequestHandle<TObject> CreateHandle(AsyncOperationHandle<TObject> asyncOperationHandle)
             {
                 return new MSResourceRequestHandle<TObject>(asyncOperationHandle);
@@ -45,6 +43,17 @@ namespace meltyStars
             public bool Equals(MSResourceRequestHandle<TObject> other)
             {
                 return asyncOperationHandle.Equals(other.AsyncOperationHandle);
+            }
+            public MSResourceRequestHandle<TObject> Subscribe(Action<TObject> onCompleted, Action<MSResourceRequestHandle<TObject>> onCompletedHandle = null)
+            {
+                this.OnCompleted = onCompleted;
+                if (onCompletedHandle != null)
+                    this.OnCompletedHandle = onCompletedHandle;
+                return this;
+            }
+            public void Dispose()
+            {
+
             }
         }
         public static MSResourceRequestHandle<TObject> LoadAsync<TObject>(string group, string name,
